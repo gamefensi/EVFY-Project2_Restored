14abb8645956739be91a870649a06c228ef33237
Qi Zhang <zq170829@gmail.com>
2021-12-11 10:39:29 -0600

    sign in and sign up modals

--- public/js/signin.js
+++ public/js/signin.js
@@ -15,19 +15,3 @@ $(function(){
       }
     })
   });
-//   $(function(){
-//     $('.hide-show').show();
-//     $('.hide-show span').addClass('show')
-    
-//     $('.hide-show span').click(function(){
-//       if( $(this).hasClass('show') ) {
-//         $(this).text('HIDE');
-//         $('input[id="l_password"]').attr('type','text');
-//         $(this).removeClass('show');
-//       } else {
-//          $(this).text('SHOW');
-//          $('input[id="l_password"]').attr('type','password');
-//          $(this).addClass('show');
-//       }
-//     })
-//   });
