45483fd543c2325f5ca9481129d0d62217697a1b
John M <mahan.john@gmail.com>
2021-12-16 19:54:39 -0800

    changed to es6

--- public/js/backToTop.js
+++ public/js/backToTop.js
@@ -4,7 +4,7 @@ var mybutton = document.getElementById("myBtn");
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
+const scrollFunction = () => {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
   } else {
@@ -13,7 +13,7 @@ function scrollFunction() {
 }
 
 // When the user clicks on the button, scroll to the top of the document
-function topFunction() {
+const topFunction = () => {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
   }
