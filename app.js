becba8aff26df97cf97389d03eff1b0521f16a23
John M <mahan.john@gmail.com>
2021-12-17 00:20:27 -0800

    more adjustments to user profile

--- app.js
+++ app.js
@@ -7,16 +7,33 @@ const bcrypt = require("bcryptjs");
 const cors = require("cors");
 const bodyParser = require('body-parser');
 const flash = require('connect-flash');
+const passportLocalMongoose = require('passport-local-mongoose');
 require("dotenv").config();
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// EXPRESS SESSION
+const expressSession = require('express-session')({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false
+});
+
 app.use(express.static('public'));
 app.use(express.json());
 app.use(cors());
 app.use(flash());
+
+// // PASSPORT SETUP
+
+// const passport = require('passport');
+
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+// CONNECT TO DB
 
 mongoose.connect(
     process.env.DB,
@@ -31,12 +48,24 @@ mongoose.connect(
       console.log("Database connected");
     }
   );
-  
+
+
+// PASSPORT LOCAL AUTHENTICATION
+
+// passport.use(User.createStrategy());
+
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+
+
+const connectEnsureLogin = require('connect-ensure-login');
+
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
+
 app.get('/', (req,res) => {
-res.render('index', { title: 'Home page' });
+res.render('index', { title: 'EVFY' });
 })
 
 app.post("/register", async (req, res) => {
@@ -94,7 +123,7 @@ app.post('/login', async (req, res) => {
         if (!user) {
           return res
             .status(400)
-            .json({ msg: "Invalid credentails" });
+            .json({ msg: "Invalid credentials" });
         }
     
         // validate password
@@ -124,6 +153,48 @@ app.post('/login', async (req, res) => {
     //   }
 });
 
+
+//EDIT USER PROFILE
+app.post("/edit", async (req, res) => {
+  try {
+    const { email, password, username, fullname} = req.body;
+    
+    // ensure email and password meet the requirements
+    if (!email || !password || !username || !fullname) {
+      return res.status(400).json({ msg: "Not all fields have been entered" });
+    }
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ msg: "The password needs to be at least 6 characters long" });
+    }
+
+    const existingEmail = await User.findOne({ email: email });
+    if (existingEmail) {
+      return res
+        .status(400)
+        .json({ msg: "An account with this email already exists" });
+    }
+
+    // Bcrypt - hashing password
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const updatedUser = new User({
+      email: email,
+      password: passwordHash,
+      username: username,
+      fullname: fullname,
+    });
+    const savedUser = await newUser.save();
+    res.json(savedUser);
+    // req.flash("success", "Successfully created the account!!");
+    res.redirect('/');
+  } catch (error) {
+    res.status(500).json({ err: error.message });
+  }
+});
 //CONVERT PUG TO HTML IN PUBLIC FOLDER
 var jade = require('pug');//require pug module
 var fs = require('fs')
--- public/js/userProfile.js
+++ public/js/userProfile.js
@@ -1,9 +1,43 @@
-$('#profileModal').on('show.bs.modal', function (event) {
+// const mongoose = require("mongoose");
+// const User = require("./models/userModel");
+
+
+
+$(document).ready(function () {
+  $('#profileModal').on('show.bs.modal', function (event) {
     var a = $(event.relatedTarget) // Button that triggered the modal
     var recipient = a.data('user') // Extract info from data-* attributes
     // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
     // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
     var modal = $(this)
-    modal.find('.modal-title').text(recipient+'\'s Profile')
+    modal.find('.modal-title').text(recipient + '\'s Profile')
     modal.find('.modal-body input').val(recipient)
+  });
+  //enable/disable editing
+  $('#editProfile input,#editProfile #saveBtn').prop('disabled', true);
+  $('#editProfile #editBtn').click(function () {
+    if ($('#editProfile input').prop('disabled', true)) {
+      $('#editProfile input:disabled,#editProfile button:disabled').removeAttr('disabled');
+      $(this).addClass('btn-secondary');
+      $(this).removeClass('btn-primary');
+      $("#editProfile #saveBtn").addClass('btn-success');
+      $("#editProfile #saveBtn").removeClass('btn-secondary');
+    } ;
+  });
+  //redisable on close
+  $('#profileModal button.close').click(function() {
+    $('#editProfile input,#editProfile #saveBtn').prop('disabled', true);
+    $("#editProfile #saveBtn").addClass('btn-secondary');
+    $("#editProfile #saveBtn").removeClass('btn-success');
+    $("#editProfile #editBtn").addClass('btn-primary');
+    $("#editProfile #editBtn").removeClass('btn-secondary');
+  });
+
+  /* show file value after file select */
+  document.querySelector('.custom-file-input').addEventListener('change', function (e) {
+    var fileName = document.getElementById("customFile").files[0].name;
+    var nextSibling = e.target.nextElementSibling
+    nextSibling.innerText = fileName
   })
+
+})
