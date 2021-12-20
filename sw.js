fae2bf1a433333bb4a968581be8e4c2305615e5d
John M <mahan.john@gmail.com>
2021-12-19 21:24:10 -0800

    add sw.js

---
+++ sw.js
@@ -0,0 +1,32 @@
+/* For sw.js */
+
+var cacheName = 'hello-pwa';
+var filesToCache = [
+  '/',
+  '/views/index.pug',
+  '/views/layout.pug',
+  '/views/footer.pug',
+  '/index.html',
+  '/public/css/style.css',
+  '/app.js',
+  '/js/main.js'
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
